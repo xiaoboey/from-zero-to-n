daf13d244b0b5446e1225f2082ab5f02e2bd85e5
xiaoqb <xiaoqb@foxmail.com>
2020-09-16 17:05:27 +0800

    feat(all): 从0到n - Spring Cloud项目实战 - 重试（重构pom.xml、网关增加重试机制）

---
+++ one/service-one/src/main/java/com/example/serviceone/ServiceOneApplication.java
@@ -0,0 +1,18 @@
+package com.example.serviceone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author xiaoqb
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServiceOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceOneApplication.class, args);
+	}
+
+}

9cae455ad7f08aec24595650a9b330d7622bc469
xiaoqb <xiaoqb@foxmail.com>
2022-01-04 13:44:00 +0800

    feat: 版本升级，spring-boot(2.3.3.RELEASE -> 2.5.8)，spring-cloud(Hoxton.SR8 -> 2020.0.5)

--- two/auth-server/src/test/java/com/example/two/authserver/AuthServerApplicationTests.java
+++ two/auth-server/src/test/java/com/example/two/authserver/AuthServerApplicationTests.java
@@ -1,25 +1,14 @@
 package com.example.two.authserver;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.mockito.BDDMockito.given;
-
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class AuthServerApplicationTests {
 
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-
 	@Test
 	public void contextLoads() {
-		System.out.println(passwordEncoder.encode("123456"));
-		given(passwordEncoder.encode("123456")).willReturn("abc");
+
 	}
 
 }
